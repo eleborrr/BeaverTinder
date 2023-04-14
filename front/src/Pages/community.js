bcb700a9e8b4cc5c096971a9872f38078138a688
AndrewKhoryakov <andrejhorakov47232@gmail.com>
2023-04-14 21:17:45 +0300

    add default pages

---
+++ front/src/Pages/community.js
@@ -0,0 +1,9 @@
+const CommunityPage = () => {
+    return(
+        <>
+        <h1>It's community page!</h1>
+        </>
+    )
+}
+
+export default CommunityPage;
---
+++ front/src/Pages/shops.js
@@ -0,0 +1,9 @@
+const ShopsPage = () => {
+    return(
+        <>
+        <h1>It's shops page!</h1>
+        </>
+    )
+}
+
+export default ShopsPage;
