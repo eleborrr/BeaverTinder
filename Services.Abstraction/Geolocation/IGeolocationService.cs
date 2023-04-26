3ddfc4177cdf35118b7ba7f0b6abd8c6c6bd67d1
Gleb Petukhov <gleb-petuhov@mail.ru>
2023-04-26 15:36:33 +0300

    obj bin folders deleted

---
+++ Services.Abstraction/Geolocation/IGeolocationService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Services.Abstraction.Geolocation;
+
+public interface IGeolocationService
+{
+    public Task AddAsync(int userId, double Latutide, double Longtitude);
+
+    public Task<UserGeolocation> GetByUserId(int userId);
+
+    public Task<IEnumerable<UserGeolocation>> GetAllAsync();
+    // отображение геолокации по координатам
+
+    // установка геолокации (?)
+
+    // 
+}
