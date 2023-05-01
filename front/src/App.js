ae68a1560739d35a188408e9edeeb1d9b7effdb3
Imarion <mihail.117@mail.ru>
2023-05-01 15:29:22 +0300

    add beaver card to like/dislike

--- front/src/App.js
+++ front/src/App.js
@@ -1,28 +1,21 @@
-import './assets/css/App.css';
+import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HeaderApp from './Components/header';
 import RegisterPage from './Pages/register';
 import LoginPage from './Pages/login';
 import AdminPage from './Pages/admin/admin';
-import HomePage from './Pages/home';
-import CommunityPage from './Pages/community';
-import ShopsPage from './Pages/shops';
-import BlogsPage from './Pages/blogs';
-import ContactPage from './Pages/contact';
+import LikePage from './Pages/LikePage';
 
 function App() {
   return (
     <>
       <HeaderApp />
       <Routes>
+        <Route path='/like' element= {<LikePage/>}/>
         <Route path='/admin' element={<AdminPage />} />
         <Route path='/login' element={<LoginPage />} />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/community' element={<CommunityPage />} />
-        <Route path='/shops' element={<ShopsPage />} />
-        <Route path='/blogs' element={<BlogsPage />} />
-        <Route path='/contact' element={<ContactPage />} />
         <Route path='*' element={<RegisterPage />} />
+        
       </Routes>
     </>
   );
---
+++ front/src/Pages/LikePage.js
@@ -0,0 +1,17 @@
+import BeaverCard from "../Components/BeaverCard";
+
+
+
+const LikePage = () =>
+{
+    return (<div>
+        {/* <BeaverCard person={{name: 'Arun', url: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1644176959380/tNxVpeCE0.png'}}> </BeaverCard> */}
+        <BeaverCard></BeaverCard>
+    </div>
+    )
+
+}
+
+
+
+export default LikePage;
