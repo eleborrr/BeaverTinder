f8797eddb0929ecd91d9f3dbd169a3be7c0536f0
AndrewKhoryakov <andrejhorakov47232@gmail.com>
2023-05-28 03:09:22 +0300

    add pages my-likes and profile

---
+++ front/src/Pages/my-likes.js
@@ -0,0 +1,174 @@
+import { useState, useEffect } from "react";
+import Cookies from 'js-cookie';
+import jwtDecode from "jwt-decode";
+import BeaverCard from "../Components/BeaverCard";
+import { axiosInstance } from "../Components/axios_server";
+import { GeoMap } from "../Components/geolocation_map";
+import './../assets/css/map_style.css'
+
+const MyLikesPage = () =>
+{
+    const token = Cookies.get('token');
+    const [likeLimit, setLikeLimit] = useState(false);
+    const [userLimit, setUserLimit] = useState(false);
+    const [geolocationAvailable, setGeolocationAvailable] = useState(false);
+    const [profile, setProfile] = useState();
+    const [long, setLong] = useState();
+    const [lant, setLant] = useState();
+
+    useEffect(() => {
+        GetNewBearer();
+    }, [])
+
+
+    function like () {
+        
+        axiosInstance.post('/like', { 
+            LikedUserId: profile.id 
+        }, {
+            headers:{
+                Authorization: `Bearer ${token}`,
+                Accept : "application/json"
+            }
+        })
+        .then(res => {
+            if(res.data.message === "Like limit!")
+            {
+                setLikeLimit(true);
+            }
+            else
+            {
+                GetNewBearer();
+            }
+            
+        })
+        .catch();
+    }
+
+    function dislike () {
+        axiosInstance.post('/dislike', { 
+            LikedUserId: profile.id 
+        }, {
+            headers:{
+                Authorization: `Bearer ${token}`,
+                Accept : "application/json"
+            }
+        })
+        .then(res => {
+            if(res.data.message === "Like limit!")
+            {
+                setLikeLimit(true);
+            }
+            else
+            {
+                GetNewBearer();
+            }
+        })
+        .catch();
+
+    }
+
+    function GetGeolocation(prof) {
+        axiosInstance.post("/geolocation",{
+            userId : prof.id
+        },
+         {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json'
+            },
+        })
+        .then(res => {
+            if (res.data){
+                if (res.data.longitude){
+                    setLong(res.data.longitude);
+                }
+                if (res.data.latitude){
+                    setLant(res.data.latitude);
+                }
+            }
+        })
+        .catch()
+    }
+    
+    function GetNewBearer() {
+    axiosInstance.get('',
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept : "application/json"
+            }
+        })
+        .then(res => {
+            if(res.data.message === "Beaver queue error")
+            {
+                setUserLimit(true);
+            }
+            else
+            {
+            setProfile(res.data);
+
+            if (res.data){
+                setGeolocationAvailable(false);
+                CheckGeolocationAvailable(jwtDecode(token)["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]);
+                GetGeolocation(res.data);
+            }
+        }
+        })
+        .catch();
+   
+    }
+
+    function CheckGeolocationAvailable(array)
+    {
+        if (Array.isArray(array)) {
+            array.some(element => {
+                if (element === "UserMoreLikesAndMap" || element === "Admin" || element === "Moderator")
+                {
+                    setGeolocationAvailable(true);
+                }
+            })
+        }
+        
+    }
+
+    return (
+    <div>
+        { userLimit? 
+        <div> 
+            Пользователи закончились, ждите новых
+        </div>
+        :
+        <div>
+        {likeLimit? <p>У вас закончились лайки</p>:
+        <div>
+            {profile ? 
+            <div>
+                <BeaverCard profile = {profile} like = {like} dislike = {dislike}></BeaverCard>
+                {geolocationAvailable?
+                <div className="div_map">
+                    <GeoMap latitude={lant ? lant : 55.81441} longitude={long ? long : 49.12068} />
+                </div>
+                :
+                <div>
+                    Купите подписку чтобы увидеть геолокацию
+                </div>
+                }
+            
+            </div>:
+            <h1>Not Found users witch liked you :(</h1>
+            }
+
+        </div>
+        }
+        </div>
+    }
+
+    </div>
+    )
+    
+}
+
+
+
+export default MyLikesPage;
---
+++ front/src/Pages/profile.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from 'react';
+import Dropzone from 'react-dropzone';
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import './../assets/css/profile.css'
+
+const Profile = () => {
+  const [changing, setChanging] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [photo, setPhoto] = useState(null);
+  const [location, setLocation] = useState('');
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  useEffect(() => {
+    // Запрос на сервер для получения текущей информации о пользователе
+    // axios.get('/api/user').then((response) => {
+    //   const { firstName, lastName, email, password, photo, location } =
+    //     response.data;
+    //   setFirstName(firstName);
+    //   setLastName(lastName);
+    //   setEmail(email);
+    //   setPhoto(photo);
+    //   setLocation(location);
+    // });
+  }, []);
+
+  function handleMapClick(event) {
+    const coords = event.get('coords');
+    setLatitude(coords[0]);
+    setLongitude(coords[1]);
+    setLocation(coords.join(', '));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Отправка данных на сервер
+    // const formData = new FormData();
+    // formData.append('firstName', firstName);
+    // formData.append('lastName', lastName);
+    // formData.append('email', email);
+    // if (photo) {
+    //   formData.append('photo', photo);
+    // }
+    // formData.append('location', location);
+    // formData.append('latitude', latitude);
+    // formData.append('longitude', longitude);
+    // axios.post('/api/user', formData).then((response) => {
+    //   console.log(response.data);
+    // });
+  };
+
+  return (
+    <section className="log-reg">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-7">
+                    <div className="log-reg-inner">
+                        <div className="section-header">
+                            <h2 className="title">Info about subscription? </h2>
+                            <p>Info? </p>
+                        </div>
+                        <div className="main-content">
+                            <form onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="firstName">First Name</label>
+                                <input
+                                type="text"
+                                name="firstName"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                                className="my-form-control"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="lastName">Last Name</label>
+                                <input
+                                type="text"
+                                name="lastName"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                                className="my-form-control"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                type="email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="my-form-control"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="photo">Photo</label>
+                                <Dropzone onDrop={(acceptedFiles) => setPhoto(acceptedFiles[0])}>
+                                {({ getRootProps, getInputProps }) => (
+                                    <div {...getRootProps()}>
+                                    <input {...getInputProps()} />
+                                    <p>Drag and drop a file here, or click to select a file</p>
+                                    </div>
+                                )}
+                                </Dropzone>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="location">Location</label>
+                                <input type="text" className="my-form-control" value={location} disabled={true}/>
+                                    {
+                                        changing?
+                                        <YMaps>
+                                            <div>
+                                                <Map onClick={handleMapClick} width="100%" height="400px" defaultState={{ center: [55.76, 37.64], zoom: 10 }}>
+                                                    {location? <Placemark geometry={location.split(", ")} />: <></>}
+                                                </Map>
+                                            </div>
+                                        </YMaps>
+                                        :
+                                        <></>
+                                    }
+                                    
+                            </div>
+                            {
+                                changing?
+                                <button className="default-btn reverse" data-toggle="modal" data-target="#email-confirm">
+                                    <span>Save</span>
+                                </button>
+                                :
+                                <button className="default-btn reverse" data-toggle="modal" data-target="#email-confirm" onClick={() => setChanging(true)}>
+                                    <span>Change</span>
+                                </button>
+                            }
+                            
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className='profile-img' style={{width:'42%'}}>
+                    <img href=''/>
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+};
+
+export default Profile;
