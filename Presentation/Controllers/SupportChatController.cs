b70b89ba9490cfc0503f0b2d9bcfdb9b3db7322e
Gleb Petukhov <gleb-petuhov@mail.ru>
2023-10-09 13:33:13 +0300

    merge

--- Presentation/Controllers/SupportChatController.cs
+++ Presentation/Controllers/SupportChatController.cs
@@ -1,6 +1,9 @@
-﻿using Contracts;
+using Contracts;
+using Contracts.Responses;
+using Contracts.Responses.Chat;
 using Domain.Entities;
 using Domain.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +13,8 @@ using Services.Abstraction;
 namespace Presentation.Controllers;
 
 [ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Route("[controller]")]
 public class SupportChatController : Controller
 {
     private readonly IServiceManager _serviceManager;
@@ -23,13 +28,38 @@ public class SupportChatController : Controller
         _userManager = userManager;
     }
     
-    [Authorize]
-    [HttpGet("history")]
-    public async Task<IActionResult> GetChatHistory([FromQuery] string secondUserId)
+    [HttpGet("/im/supportChat")]
+    public async Task<JsonResult> Chat([FromQuery] string username)
+    {
+        try
+        {
+            var curUserId = User.Claims.FirstOrDefault(c => c.Type == "Id").Value;
+            var receiver = await _userManager.FindByNameAsync(username);
+            var sender = await _userManager.FindByIdAsync(curUserId);
+
+            //TODO check for curUserId null
+            var res = await _serviceManager.ChatService.GetChatById(sender.Id, receiver.Id);
+            var model = new SingleChatGetResponse()
+            {
+                RecieverName = username,
+                SenderName = sender.UserName,
+                RoomName = res.Name
+            };
+            return Json(model);
+        }
+        catch (Exception exception)
+        {
+            return Json(new FailResponse(false, exception.Message, 400));
+        }
+    }
+    
+    [HttpGet("/history")]
+    public async Task<JsonResult> GetChatHistory([FromQuery] string username)
     {
         var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
-        var history = await _serviceManager.SupportChatService.GetChatHistory(user!.Id, secondUserId);
-        return Ok(history);
+        var secondUser = await _userManager.FindByNameAsync(username);
+        var history = await _serviceManager.SupportChatService.GetChatHistory(user!.Id, secondUser!.Id);
+        return Json(history);
     }
 
     [HttpGet("send")]
--- front/src/Components/window_connect_with_admin.js
+++ front/src/Components/window_connect_with_admin.js
@@ -1,28 +1,112 @@
 import './../assets/css/chat_with_admin.css'
-import React, { useState } from 'react';
+import jwtDecode from "jwt-decode";
+import { axiosInstance } from "../Components/axios_server";
+import 'https://cdnjs.cloudflare.com/ajax/libs/microsoft-signalr/6.0.1/signalr.js';
+import * as signalR from "@microsoft/signalr";
+import Cookies from "js-cookie";
+import React, {useCallback, useEffect, useState} from 'react';
+import { useNavigate, useParams } from "react-router-dom";
 
 const ChatWindow = () => {
+  const navigate = useNavigate();
   const [messages, setMessages] = useState([]);
+  const token = Cookies.get('token');
   const [newMessage, setNewMessage] = useState('');
   const [isOpen, setIsOpen] = useState(false);
+  const { nickname } = useParams();
 
-  const handleSendMessage = () => {
-    if (newMessage.trim() !== '') {
+  useEffect(() => {
+    if (!token){
+        navigate("/login");
+    }
+    }, [navigate, token])
+
+
+  const handleSendMessage = (msg) => {
+    if (msg.trim() !== '') {
         const mess = { 
-            message : newMessage,
+            message : msg,
             author: 'me' 
         }
         const m2 = {
-            message : newMessage,
+            message : msg,
             author: 'to'
         }
         setMessages([...messages, mess, m2]);
-        setNewMessage('');                        
     }
   };
   const togglePopup = () => {
     setIsOpen(!isOpen);
   }
+
+    const [message, setMessage] = useState('');
+    const callbackSignalR = useCallback((roomData) => {
+    
+        let connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:7015/chatHub").build();
+    
+        connection.on("ReceivePrivateMessage", function (user, message){
+            var elem = document.createElement("div");
+            var author = document.createElement("span");
+            var content = document.createElement("span");
+            if(user === nickname){
+                elem.className="message-from";
+    
+                author.className = "message-from";
+            }
+            else{
+                elem.className="message-to";
+    
+                author.className = "message-to";
+            }
+            author.textContent = user + ":";
+    
+            content.className = "message-text";
+            content.textContent = message;
+    
+            elem.appendChild(author);
+            elem.appendChild(content);
+    
+            document.getElementById("messagesList").appendChild(elem);
+    
+        });
+    
+        document.getElementById("sendButton").addEventListener("click", function (event) {
+            var message = document.getElementById("messageInput").value;
+            connection.invoke("SendPrivateMessage", `${roomData.senderName}`, message, `${roomData.recieverName}`, `${roomData.roomName}`).catch(function (err) {
+                return console.error(err.toString());
+            });
+            event.preventDefault();
+        });
+    }, [nickname])
+
+    useEffect(() => {
+        let room;
+        axiosInstance.get(`/im/supportChat?username=${nickname}`,
+            {
+                headers:{
+                    Authorization: `Bearer ${token}`,
+                    Accept : "application/json"
+                }
+            })
+            .then(response => {
+                room = response.data; // выводим данные, полученные из сервера
+                callbackSignalR(room);
+            })
+            .catch();
+        let messages;
+        axiosInstance.get(`/history?username=${nickname}`,
+            {
+                headers:{
+                    Authorization: `Bearer ${token}`,
+                    Accept : "application/json"
+                }
+            })
+            .then(response => {
+                messages = response.data; // выводим данные, полученные из сервера
+                messages.forEach(msg => handleSendMessage(msg))
+            })
+            .catch();
+    }, [callbackSignalR, nickname, token])
 
   return (
     <div>
