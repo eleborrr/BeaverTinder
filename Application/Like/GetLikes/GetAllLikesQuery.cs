34c48ce4600af90f2cda5c9ee05465b896174051
Imarion <mihail.117@mail.ru>
2023-11-11 20:03:21 +0300

    add Like cqrs

---
+++ Application/Like/GetLikes/GetAllLikesQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using Services.Abstraction.Cqrs.Queries;
+
+namespace Application.Like.GetLikes;
+
+public class GetAllLikesQuery : IQuery<IEnumerable<Domain.Entities.Like>>
+{
+    
+}
