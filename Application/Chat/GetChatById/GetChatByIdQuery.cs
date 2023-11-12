a162ac9f036bc8110b585a921bd7e571e18ac547
AndrejHoryakov <andrejhorakov47232@gmail.com>
2023-11-12 16:01:00 +0300

    [add] GetChatByIdQuery

---
+++ Application/Chat/GetChatById/GetChatByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Services.Abstraction.Cqrs.Queries;
+
+namespace Application.Chat.GetChatById;
+
+public class GetChatByIdQuery : IQuery<Room?>
+{
+    public string CurrUserId { get; set; } = default!;
+    public string UserId { get; set; } = default!;
+}
