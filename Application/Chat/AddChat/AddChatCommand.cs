4a2e24cc2c1b9fa797a5f97405cd0c59ba7f0190
AndrejHoryakov <andrejhorakov47232@gmail.com>
2023-11-12 16:00:21 +0300

    [add] AddChatCommand

---
+++ Application/Chat/AddChat/AddChatCommand.cs
@@ -0,0 +1,9 @@
+﻿using Services.Abstraction.Cqrs.Commands;
+
+namespace Application.Chat.AddChat;
+
+public class AddChatCommand : ICommand<Guid>
+{
+    public string FirstUserId { get; set; } = default!;
+    public string SecondUserId { get; set; } = default!;
+}
---
+++ Application/Chat/AddChat/AddChatHandler.cs
@@ -0,0 +1,37 @@
+﻿using Contracts.Dto.MediatR;
+using Domain.Entities;
+using Domain.Repositories;
+using Services.Abstraction.Cqrs.Commands;
+
+namespace Application.Chat.AddChat;
+
+public class AddChatHandler : ICommandHandler<AddChatCommand, Guid>
+{
+    private readonly IRepositoryManager _repositoryManager;
+
+    public AddChatHandler(IRepositoryManager repositoryManager)
+    {
+        _repositoryManager = repositoryManager;
+    }
+    public async Task<Result<Guid>> Handle(AddChatCommand request, CancellationToken cancellationToken)
+    {
+        var chatId = Guid.NewGuid();
+        var room = new Room
+        {
+            Id = chatId.ToString(),
+            FirstUserId = request.FirstUserId,
+            SecondUserId = request.SecondUserId,
+            Name = Guid.NewGuid().ToString()
+        };
+        try
+        {
+            await _repositoryManager.RoomRepository.AddAsync(
+                room);
+            return new Result<Guid>(chatId, true);
+        }
+        catch (Exception e)
+        {
+            return new Result<Guid>(Guid.Empty, false, e.Message);
+        }
+    }
+}
