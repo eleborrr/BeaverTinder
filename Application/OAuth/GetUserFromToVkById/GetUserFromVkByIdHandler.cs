d23068536d4d605e308bcf9ee6b33d97d4231f31
AndrejHoryakov <andrejhorakov47232@gmail.com>
2023-11-13 11:43:17 +0300

    [add] GetUserFromToVkByIdQuery

---
+++ Application/OAuth/GetUserFromToVkById/GetUserFromVkByIdHandler.cs
@@ -0,0 +1,34 @@
+﻿using Contracts.Dto.MediatR;
+using Domain.Entities;
+using Domain.Repositories;
+using Services.Abstraction.Cqrs.Queries;
+
+namespace Application.OAth.GetUserFromToVkById;
+
+public class GetUserFromVkByIdHandler : IQueryHandler<GetUserFromVkByIdQuery, UserToVk?>
+{
+    private readonly IRepositoryManager _repositoryManager;
+
+    public GetUserFromVkByIdHandler(IRepositoryManager repositoryManager)
+    {
+        _repositoryManager = repositoryManager;
+    }
+
+    public async Task<Result<UserToVk?>> Handle(
+        GetUserFromVkByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var vkUser = await _repositoryManager.UserToVkRepository.GetByIdAsync(request.VkId);
+            if (vkUser is null)
+                return new Result<UserToVk?>(null, false, "User not found");
+            return new Result<UserToVk?>(vkUser, true);
+        }
+        catch (Exception e)
+        {
+            return new Result<UserToVk?>(null, false, e.Message);
+        }
+    }
+            
+}
---
+++ Application/OAuth/GetUserFromToVkById/GetUserFromVkByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using Domain.Entities;
+using Services.Abstraction.Cqrs.Queries;
+
+namespace Application.OAth.GetUserFromToVkById;
+
+public record GetUserFromVkByIdQuery(string VkId) : IQuery<UserToVk?>;
