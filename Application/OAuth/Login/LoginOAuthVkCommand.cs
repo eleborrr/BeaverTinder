bdb47cbf31e4c2d59f158a348578fd18fd8d919b
AndrejHoryakov <andrejhorakov47232@gmail.com>
2023-11-13 11:39:59 +0300

    [add] LoginOAuthVkCommand

---
+++ Application/OAuth/Login/LoginOAuthVkCommand.cs
@@ -0,0 +1,7 @@
+﻿using Contracts.Dto.Authentication.Login;
+using Domain.Entities;
+using Services.Abstraction.Cqrs.Commands;
+
+namespace Application.OAth.Login;
+
+public record LoginOAuthVkCommand(User SignedUser) : ICommand<LoginResponseDto>;
---
+++ Application/OAuth/Login/LoginOAuthVkValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.OAth.Login;
+
+public class LoginOAuthVkValidator : AbstractValidator<LoginOAuthVkCommand>
+{
+    public LoginOAuthVkValidator()
+    {
+        RuleFor(c => c.SignedUser).NotNull();
+    }
+}
