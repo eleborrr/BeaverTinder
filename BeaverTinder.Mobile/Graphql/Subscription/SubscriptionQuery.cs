b141cf58079b09c575e5d2ac0e6e90ce5c64e910
Gleb Petukhov <gleb-petuhov@mail.ru>
2024-05-03 19:13:07 +0300

    [add] graphql subscription method

---
+++ BeaverTinder.Mobile/Graphql/Subscription/SubscriptionQuery.cs
@@ -0,0 +1,25 @@
+﻿using BeaverTinder.Application.Features.Subscription.GetAllSubscriptions;
+using BeaverTinder.Shared.Dto.Subscription;
+using BeaverTinder.Subscription.Core.Entities;
+using MediatR;
+
+namespace BeaverTinder.Mobile.Graphql.Subscription;
+
+public class SubscriptionQuery
+{
+    private readonly IMediator _mediator;
+
+    public SubscriptionQuery(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task<List<SubscriptionInfoDto>> GetAllSubscriptions()
+    {
+        var query = new GetAllSubscriptionsQuery();
+        var subscriptions = (await _mediator.Send(query)).Value;
+        if (subscriptions is null)
+            return new List<SubscriptionInfoDto>();
+        return subscriptions.ToList();
+    }
+}
