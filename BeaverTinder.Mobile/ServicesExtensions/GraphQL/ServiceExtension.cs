ede8ef33cae9335996b7b0492ed41e74575953e0
Gleb Petukhov <gleb-petuhov@mail.ru>
2024-05-04 17:26:55 +0300

    [add] server error filter; auth; beaver search 2 queries

--- BeaverTinder.Mobile/ServicesExtensions/GraphQL/ServiceExtension.cs
+++ BeaverTinder.Mobile/ServicesExtensions/GraphQL/ServiceExtension.cs
@@ -1,16 +1,48 @@
-﻿namespace BeaverTinder.Mobile.ServicesExtensions.GraphQL;
+﻿using System.Security.Claims;
+using BeaverTinder.Mobile.Helpers;
+using BeaverTinder.Mobile.Helpers.Filters;
+using HotChocolate;
+using HotChocolate.AspNetCore;
+using HotChocolate.AspNetCore.Authorization;
+
+namespace BeaverTinder.Mobile.ServicesExtensions.GraphQL;
 
 public static class ServicesCollectionExtension
 {
     public static IServiceCollection AddGraphQL(this IServiceCollection services,
-        IConfiguration configuration)
+        IConfiguration configuration, IWebHostEnvironment environment)
     {
         services
-            .AddGraphQLServer();
+            .AddGraphQLServer()
+            .AddErrorFilter(provider =>
+            {
+                return new ServerErrorFilter(
+                    provider.GetRequiredService<ILogger<ServerErrorFilter>>(),
+                    environment);
+            })
+            .AddAuthorization(options => 
+            {
+                options.AddPolicy("OnlyMapSubs", policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, "UserMoreLikesAndMap", "Moderator", "Admin");
+                });
+                options.AddPolicy("OnlyLikeSubs", policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, "UserMoreLikes", "Moderator", "Admin");
+                });
+                options.AddPolicy("OnlyForAdmins", policy => {
+                    policy.RequireClaim(ClaimTypes.Role, "Admin");
+                });
+                options.AddPolicy("OnlyForModerators", policy => {
+                    policy.RequireClaim(ClaimTypes.Role, "Moderator", "Admin");
+     
+                });
+            })
+
             // .AddQueryType<TeacherQuery>()
             // .AddMutationType<TeacherMutation>()
             // .AddSubscriptionType<TeacherSubscription>();
-
+            ;
         return services;
     }
     
