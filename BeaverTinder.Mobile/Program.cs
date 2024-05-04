f84de3a19e70ef4944663d818bee05f42bc5bc0a
Imarion <mihail.117@mail.ru>
2024-05-04 19:49:36 +0300

    [upd]

--- BeaverTinder.Mobile/Program.cs
+++ BeaverTinder.Mobile/Program.cs
@@ -5,13 +5,15 @@ using BeaverTinder.Mobile.ServicesExtensions.GraphQL;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGraphQL(builder.Configuration, builder.Environment);
+builder.Services.AddCustomAuth(builder.Configuration);
 builder.Services.AddCustomServices();
 
-builder.Services.AddMediatR(configuration =>
-{
-    configuration.RegisterServicesFromAssembly(AplicationAssemblyReference.Assembly);
-    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
-});
+// builder.Services.AddMediatR(configuration =>
+// {
+//     configuration.RegisterServicesFromAssembly(AplicationAssemblyReference.Assembly);
+//     configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+// });
+
 
 var app = builder.Build();
 
