376d1bce6907211a9dd6b93154d6ff18c563c276
Gleb Petukhov <gleb-petuhov@mail.ru>
2024-05-04 17:51:53 +0300

    [add] register mediatr

--- BeaverTinder.Mobile/Program.cs
+++ BeaverTinder.Mobile/Program.cs
@@ -1,8 +1,15 @@
+using BeaverTinder.Application.Helpers;
 using BeaverTinder.Mobile.ServicesExtensions.GraphQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGraphQL(builder.Configuration, builder.Environment);
+
+builder.Services.AddMediatR(configuration =>
+{
+    configuration.RegisterServicesFromAssembly(AplicationAssemblyReference.Assembly);
+    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
