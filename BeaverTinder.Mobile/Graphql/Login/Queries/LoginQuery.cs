3912298f58497b357be2e9debeb7f746334a4ada
Imarion <mihail.117@mail.ru>
2024-05-04 17:54:40 +0300

    [ADD] queries for login/register(notimplemented)

---
+++ BeaverTinder.Mobile/Graphql/Login/Queries/LoginQuery.cs
@@ -0,0 +1,22 @@
+﻿using BeaverTinder.Application.Dto.Authentication.Login;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeaverTinder.Mobile.Graphql.Login.Queries;
+
+public class LoginQuery
+{
+    private readonly IMediator _mediator;
+
+    public LoginQuery(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+
+    public async Task<JsonResult> Login(LoginRequestDto model)
+    {
+        // return Json(await _serviceManager.AccountService.Login(model, ModelState));
+        throw new NotImplementedException();
+    }
+}
---
+++ BeaverTinder.Mobile/Graphql/Registration/Queries/RegisterQuery.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http.Results;
+using BeaverTinder.Application.Dto.Authentication.Register;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeaverTinder.Mobile.Graphql.Registration.Queries;
+
+public class RegisterQuery
+{
+    private readonly IMediator _mediator;
+
+    public RegisterQuery(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+
+    public async Task<JsonResult> Register(RegisterRequestDto model)
+    {
+        // return Json(await _serviceManager.AccountService.Register(model, ModelState));
+        throw new NotImplementedException();
+    }
+}
