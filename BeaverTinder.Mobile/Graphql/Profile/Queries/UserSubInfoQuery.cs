fb0cf0ae3c9c51a8db810d6da9c92ccecbb09c17
Gleb Petukhov <gleb-petuhov@mail.ru>
2024-05-05 17:02:42 +0300

    merged

---
+++ BeaverTinder.Mobile/Graphql/Profile/Queries/UserSubInfoQuery.cs
@@ -0,0 +1,35 @@
+﻿using BeaverTinder.Application.Features.Subscription.GetUsersActiveSubscription;
+using BeaverTinder.Shared.Dto.Subscription;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeaverTinder.Mobile.Graphql.Profile.Queries;
+
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class UserSubInfoQuery
+{
+    private readonly IMediator _mediator;
+
+    public UserSubInfoQuery(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task<UserSubscriptionDto> GetUserSubInformation(
+        [FromQuery] string userId,
+        CancellationToken cancellationToken)
+    {
+        var subInfo = (await _mediator.Send(
+            new GetUsersActiveSubscriptionQuery(userId),
+            cancellationToken)).Value;
+        
+        var model = new UserSubscriptionDto()
+        {
+            Name = subInfo!.Name,
+            Expires = subInfo.Expires
+        };
+        return model;
+    }
+}
