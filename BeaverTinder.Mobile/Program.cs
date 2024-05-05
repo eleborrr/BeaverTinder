cdb7cdb09ab6a7f9b8fd52beb157f3cfc640fc22
Gleb Petukhov <gleb-petuhov@mail.ru>
2024-05-05 17:01:40 +0300

    merged

--- BeaverTinder.Mobile/Program.cs
+++ BeaverTinder.Mobile/Program.cs
@@ -1,16 +1,21 @@
 
+using BeaverTinder.API.ServicesExtensions.Auth;
+using BeaverTinder.API.ServicesExtensions.Services;
 using BeaverTinder.Application.Helpers;
 using BeaverTinder.Mobile.ServicesExtensions.GraphQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGraphQL(builder.Configuration, builder.Environment);
+builder.Services.AddCustomAuth(builder.Configuration);
+builder.Services.AddCustomServices(builder.Configuration);
 
-builder.Services.AddMediatR(configuration =>
-{
-    configuration.RegisterServicesFromAssembly(AplicationAssemblyReference.Assembly);
-    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
-});
+// builder.Services.AddMediatR(configuration =>
+// {
+//     configuration.RegisterServicesFromAssembly(AplicationAssemblyReference.Assembly);
+//     configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+// });
+
 
 var app = builder.Build();
 
