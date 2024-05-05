0189378c94886eca355ca0cd319f5307a14c8da5
Gleb Petukhov <gleb-petuhov@mail.ru>
2024-05-05 17:02:19 +0300

    merged

--- BeaverTinder.Mobile/Program.cs
+++ BeaverTinder.Mobile/Program.cs
@@ -1,28 +1,76 @@
-
 using BeaverTinder.API.ServicesExtensions.Auth;
+using BeaverTinder.API.ServicesExtensions.Grpc;
+using BeaverTinder.API.ServicesExtensions.SecurityAndCors;
 using BeaverTinder.API.ServicesExtensions.Services;
 using BeaverTinder.Application.Helpers;
+using BeaverTinder.Domain.Entities;
+using BeaverTinder.Infrastructure.Database;
 using BeaverTinder.Mobile.ServicesExtensions.GraphQL;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddMemoryCache();
+builder.Services.AddSignalR();
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMvc();
 builder.Services.AddGraphQL(builder.Configuration, builder.Environment);
-builder.Services.AddCustomAuth(builder.Configuration);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+ options.UseSqlServer(builder.Configuration.GetConnectionString("BeaverTinderDatabase"));
+ options.EnableSensitiveDataLogging();
+});
+builder.Services.AddIdentity<User, Role>(
+     options =>
+     {
+         options.SignIn.RequireConfirmedAccount = false; // change in prod
+         options.SignIn.RequireConfirmedEmail = true;  // change in prod
+     })
+ .AddDefaultTokenProviders()
+ .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.Configure<DataProtectionTokenProviderOptions>(
+ o => o.TokenLifespan = TimeSpan.FromHours(24));
+
+
+builder.Services.ConfigureGrpc(builder.Configuration);
+
 builder.Services.AddCustomServices(builder.Configuration);
 
-// builder.Services.AddMediatR(configuration =>
-// {
-//     configuration.RegisterServicesFromAssembly(AplicationAssemblyReference.Assembly);
-//     configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
-// });
+builder.Services.AddMediatR(configuration =>
+{
+    configuration.RegisterServicesFromAssembly(AplicationAssemblyReference.Assembly);
+    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
+builder.Services.AddCustomAuth(builder.Configuration);
+
+const string testSpesific = "testSpesific";
+
+builder.Services.AddCustomCors(testSpesific);
 
 var app = builder.Build();
 
-app.UseAuthentication();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(testSpesific);
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication(); 
 app.UseAuthorization();
 
-app.UseWebSockets();
-app.MapGraphQL((PathString)"/graphql");
+app.MapControllers();
+
+app.MapGraphQL(new PathString("/graphql"));
 
 app.Run();
